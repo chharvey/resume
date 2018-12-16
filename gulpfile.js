cd6ba3641861b4cd5aa9db8306f3b6406ed76b7b
Chris Harvey <chrisharvey2pi@gmail.com>
2018-12-16 18:49:24 -0500

    add test

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,43 @@
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
 const gulp = require('gulp')
 const typescript = require('gulp-typescript')
 // require('typescript') // DO NOT REMOVE … peerDependency of `gulp-typescript`
+
+const mkdirp = require('make-dir')
+
+const xjs = require('extrajs-dom')
 
 const tsconfig = require('./tsconfig.json')
 
 
 function dist() {
-	return gulp.src(['./src/**/*.ts'])
+	return gulp.src('./src/**/*.ts')
 		.pipe(typescript(tsconfig.compilerOptions))
 		.pipe(gulp.dest('./dist/'))
 }
 
 function test_out() {
+	return Promise.resolve(null)
 }
 
-function test_run() {
+async function test_run() {
+	const {requireOther} = require('schemaorg-jsd/lib/requireOther.js')
+
+	const resume = require('./')
+	const DATA = requireOther('./test/src/sample-data.jsonld')
+
+	let contents = new xjs.Document(await resume(DATA)).innerHTML()
+	await mkdirp('./test/out/')
+	return util.promisify(fs.writeFile)(path.resolve(__dirname, './test/out/test.html'), contents, 'utf8')
 }
 
 const test = gulp.series(test_out, test_run)
 
 function docs() {
+	return Promise.resolve(null)
 }
 
 const build = gulp.parallel(
